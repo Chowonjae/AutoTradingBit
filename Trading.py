679f6dbaeccfbd1ee26c4f8752751be08ed09573
Chowonjae <sunmoon1030@gmail.com>
2021-04-24 19:08:13 +0900

    4/24 2st. cbt - order_state bot main.py에서 Trading.py로 이동 - stop_loss 함수 오류로 비활성화

--- Trading.py
+++ Trading.py
@@ -6,6 +6,7 @@ import bot.slackBot as bot
 
 upbit = key.api_key()
 
+
 # 매수
 def buy_crypto_currency(ticker):
     orderbook = pu.get_orderbook(ticker)
@@ -14,6 +15,7 @@ def buy_crypto_currency(ticker):
     volume = '%.8f' % a
     upbit.buy_limit_order(ticker, sell_price, volume)
 
+
 # 매도
 def sell_crypto_currency(ticker):
     coin = upbit.get_balance(ticker)
@@ -21,9 +23,11 @@ def sell_crypto_currency(ticker):
     buy_price = orderbook[0]['orderbook_units'][0]['ask_price']
     upbit.sell_limit_order(ticker, buy_price, coin)
 
+
 # 취소
 def bought_crypto_cancel(uuid):
     upbit.cancel_order(uuid)
+
 
 # 미체결 주문
 def order_state(coin):
@@ -32,16 +36,16 @@ def order_state(coin):
     if len(state_wait) > 0:
         for i in range(len(state_wait)):
             orderId = state_wait[i]['uuid']
-            bought_crypto_cancel(orderId)   # 미체결 주문 취소
-            print("주문 ", state_wait[i]['market'], "이 ", state_wait[i]['volume'], "개 취소 되었습니다.")
+            bought_crypto_cancel(orderId)  # 미체결 주문 취소
+            bot.cancel_bot(coin)
+
 
 # stop-loss
-def stop_loss(coin):
-    state_done = upbit.get_order(coin, state="done")
-    current_price = pu.get_current_price(coin)
-    if len(state_done) > 0:
-        for i in range(len(state_done)):
-            if state_done[i]['ord_type'] in "limit":
-                current_price < state_done[i]['price'] * 0.1
-
-                return True
+# def stop_loss(coin):
+#     state_done = upbit.get_order(coin, state="done")
+#     current_price = pu.get_current_price(coin)
+#     if len(state_done) > 0:
+#         for i in range(len(state_done)):
+#             if state_done[i]['ord_type'] in "limit":
+#                 current_price < float(state_done[i]['price'] * 0.1)
+#                 return True
--- main.py
+++ main.py
@@ -12,16 +12,16 @@ import bot.slackBot as bot
 
 
 if __name__ == '__main__':
-    # try:
+    try:
         upbit = key.api_key()
-        coin_list = ['KRW-ADA']
+        coin_list = ['KRW-BTC']
         bought_list = []
-        target_buy_count = 2                    # 주문예정 암호화폐 개수
+        target_buy_count = 1                    # 주문예정 암호화폐 개수
         buy_percent = 0.40                      # 화폐별 구매 비율
         holding_cash = upbit.get_balance("KRW") # 보유한 현금
         bring_balances = upbit.get_balances()   # 보유한 암호화폐 조회
         buy_amount = holding_cash * buy_percent # 화폐당 주문 가능 금액
-        holding_cash_message = '주문 가능 금액 ' + str(holding_cash) + '\n'
+        holding_cash_message = '주문 가능 금액 ' + str(holding_cash) + '원 \n'
         buy_percent_message = '암호화폐별 주문 비율 : ' + str(buy_percent) + '\n'
         buy_amount_message = '암호화폐별 주문 금액 : ' + str(buy_amount) + '\n'
         start_time_message = '시작 시간 :: ' + str(datetime.datetime.now().strftime('%m/%d %H:%M:%S')) + '\n'
@@ -58,18 +58,17 @@ if __name__ == '__main__':
             # 주문 취소
             if open + datetime.timedelta(minutes=20) < now:  # 10분이 지난 후 미체결 주문 취소
                 trading.order_state(coin)
-                bot.cancel_bot(coin)
                 time.sleep(1)
 
-            if trading.stop_loss(coin) is True:
-                trading.sell_crypto_currency(coin)
-                bot.stop_loss_bot(coin)
-                time.sleep(1)
+            # if trading.stop_loss(coin) is True:
+            #     trading.sell_crypto_currency(coin)
+            #     bot.stop_loss_bot(coin)
+            #     time.sleep(1)
 
             if open - datetime.timedelta(seconds=10) < now < open - datetime.timedelta(seconds=1):  # 9시에 전량 매도
                 trading.sell_crypto_currency(coin)
                 bot.sell_bot(coin)
 
             time.sleep(1)
-    # except Exception as e:
-    #     bot.error_bot(e)
+    except Exception as e:
+        bot.error_bot(e)
